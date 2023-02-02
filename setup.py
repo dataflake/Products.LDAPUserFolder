7bdb6668d4e9a7c51adf60aa58ad0587502003e9
Jens Vagelpohl <jens@plyp.com>
2023-02-02 17:38:24 +0100

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(name):
 
 
 setup(name='Products.LDAPUserFolder',
-      version='5.0',
+      version='5.1.dev0',
       description='A LDAP-enabled Zope user folder',
       long_description=read('README.rst'),
       classifiers=[
